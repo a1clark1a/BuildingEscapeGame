f4d8cb0f2460f0ac7d80531ccf7a9e52a0700280
Clark <a1clark1a@gmail.com>
2018-02-14 16:01:06 -0800

    4th Commit

--- EscapeGame/Source/EscapeGame/PositionReport.cpp
+++ EscapeGame/Source/EscapeGame/PositionReport.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PositionReport.h"
+#include "GameFramework/Actor.h"
 
 
 // Sets default values for this component's properties
@@ -10,7 +11,6 @@ UPositionReport::UPositionReport()
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
 
-	// ...
 }
 
 
@@ -18,8 +18,9 @@ UPositionReport::UPositionReport()
 void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
-
-	// ...
+	FString objName = GetOwner()->GetName();
+	FString objPos =FVector(GetOwner()->GetActorLocation().X, GetOwner()->GetActorLocation().Y, GetOwner()->GetActorLocation().Z).ToString();
+	UE_LOG(LogTemp, Warning, TEXT("%s is at %s"), *objName, *objPos);
 	
 }
 
